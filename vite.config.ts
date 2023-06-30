b4349977945e52676e8e043991ba9f52309b48df
Luis Osio Chico <72286405+LOsioChico@users.noreply.github.com>
2023-06-29 19:21:14 -0500

    feat: Add vitest configuration for testing

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import react from '@vitejs/plugin-react-swc'
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vitest/config'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,5 +9,9 @@ export default defineConfig({
   },
   preview: {
     port: 3000,
+  },
+  test: {
+    globals: true,
+    environment: 'jsdom',
   },
 })
