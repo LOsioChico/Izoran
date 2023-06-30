c033f74b71a6c7c233b8f9eb8331394245e98a3a
Luis Osio Chico <72286405+LOsioChico@users.noreply.github.com>
2023-06-29 19:35:53 -0500

    feat(index): add ReactQueryProvider and App component

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { App } from './app/App'
 import './index.css'
+import { ReactQueryProvider } from './infrastructure/state'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <>Testing</>
+    <ReactQueryProvider>
+      <App />
+    </ReactQueryProvider>
   </React.StrictMode>
 )
