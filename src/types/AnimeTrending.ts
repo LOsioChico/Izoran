f6aec206997dec7b9a4b4ca006a26ebe26224985
Luis Osio Chico <72286405+LOsioChico@users.noreply.github.com>
2023-06-30 18:27:43 -0500

    feat: add AnimeTrending types

---
+++ src/types/AnimeTrending.ts
@@ -0,0 +1,34 @@
+export interface AnimeResponse {
+  currentPage: number
+  hasNextPage: boolean
+  results: Anime[]
+}
+
+export interface Anime {
+  id: string
+  title: Title
+  image: string
+  description: string
+  status: Status
+  cover: string
+  rating: number | null
+  releaseDate: number | null
+  color: string | null
+  genres: string[]
+  totalEpisodes: number | null
+  duration: number | null
+  type: 'TV' | 'ONA'
+}
+
+export enum Status {
+  Completed = 'Completed',
+  NotYetAired = 'Not yet aired',
+  Ongoing = 'Ongoing',
+}
+
+export interface Title {
+  romaji: string
+  english: null | string
+  native: string
+  userPreferred: string
+}
