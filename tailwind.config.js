096c0193383d344e6f59bc6ee55534de9c760d2d
Luis Osio Chico <72286405+LOsioChico@users.noreply.github.com>
2023-06-30 05:21:05 -0500

    feat: Add new color 'darkLight' to theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: ['./index.html', './src/**/*.{ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        darkLight: '#2a2c31',
+      },
+    },
   },
   plugins: [],
 }
