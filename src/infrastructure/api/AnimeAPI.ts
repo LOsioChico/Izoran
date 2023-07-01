524ef7b48255dfb9b8bc5ab542259b165b6c78bb
Luis Osio Chico <72286405+LOsioChico@users.noreply.github.com>
2023-07-01 03:02:19 -0500

    feat(api): add pagination support to getTrending and getPopular functions

--- src/infrastructure/api/AnimeAPI.ts
+++ src/infrastructure/api/AnimeAPI.ts
@@ -1,8 +1,30 @@
 import { axiosInstance } from './axios'
-
 export const AnimeAPI = {
-  getTrending: async () => {
-    const { data } = await axiosInstance.get('/trending')
-    return data
+  getTrending: async (page = 1, perPage = 15) => {
+    try {
+      const { data } = await axiosInstance.get('/trending', {
+        params: {
+          page,
+          perPage,
+        },
+      })
+      return data
+    } catch (error) {
+      if (error instanceof Error) throw new Error(error.message)
+    }
+  },
+
+  getPopular: async (page = 1, perPage = 15) => {
+    try {
+      const { data } = await axiosInstance.get('/popular', {
+        params: {
+          page,
+          perPage,
+        },
+      })
+      return data
+    } catch (error) {
+      if (error instanceof Error) throw new Error(error.message)
+    }
   },
 }
