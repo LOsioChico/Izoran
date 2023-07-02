b57eea8edd1cacee96ee10f4cd2c039ff5223bc8
Luis Osio Chico <72286405+LOsioChico@users.noreply.github.com>
2023-07-01 23:49:01 -0500

    feat: Add authentication functionality to Navbar buttons

--- src/presentation/shared/Layout/Navbar/Navbar.tsx
+++ src/presentation/shared/Layout/Navbar/Navbar.tsx
@@ -1,8 +1,11 @@
+import { MainButton } from '@/app/components'
+import { AuthType, useAuthStore } from '@/infrastructure/state'
 import { NavbarLinks } from '.'
-import { IzoranLogo, MainButton } from '../..'
+import { IzoranLogo } from '../..'
 import { NavbarSearch } from './NavbarSearch'
 
 export const Navbar: React.FC = () => {
+  const { setAuthType } = useAuthStore()
   return (
     <div className='fixed z-10 flex h-20 w-full items-center justify-around border-b-2 border-b-neutral-50 border-opacity-10 bg-darkLight bg-opacity-90 text-white'>
       <div className='relative -left-16 w-40 scale-[25%] cursor-pointer'>
@@ -19,8 +22,20 @@ export const Navbar: React.FC = () => {
       <NavbarSearch />
 
       <div className='flex items-center gap-4 text-sm font-semibold'>
-        <MainButton text='Log In' variant='primary' />
-        <MainButton text='Get Started' variant='secondary' />
+        <MainButton
+          text='Log In'
+          variant='black'
+          onClick={() => {
+            setAuthType(AuthType.LOGIN)
+          }}
+        />
+        <MainButton
+          text='Get Started'
+          variant='white'
+          onClick={() => {
+            setAuthType(AuthType.REGISTER)
+          }}
+        />
       </div>
     </div>
   )
