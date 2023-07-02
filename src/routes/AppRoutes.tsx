cc1613c20de6fa2b5b55b8a129ca9062759f85fc
Luis Osio Chico <72286405+LOsioChico@users.noreply.github.com>
2023-07-01 23:45:32 -0500

    refactor: Move routes to src/routes

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,10 +1,14 @@
+import { Auth } from '@/app/auth'
+import { useAuthStore } from '@/infrastructure/state'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import { HomePage } from './pages'
-import { Layout } from './shared/Layout'
+import { HomePage } from '../presentation/pages'
+import { Layout } from '../presentation/shared/Layout'
 
 export const AppRoutes: React.FC = () => {
+  const { user } = useAuthStore()
   return (
     <BrowserRouter>
+      {user === undefined && <Auth />}
       <Routes>
         <Route element={<Layout />}>
           <Route path='/' element={<HomePage />} />
