a3b56f47c66fd865e1eb4b30c080ffde9ba09a4a
Luis Osio Chico <72286405+LOsioChico@users.noreply.github.com>
2023-07-01 23:48:38 -0500

    chore: update import path for AppRoutes

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import { ReactQueryProvider } from './infrastructure/state'
-import { AppRoutes } from './presentation/AppRoutes'
+import { AppRoutes } from './routes/AppRoutes'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
