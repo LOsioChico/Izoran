362343e15f5efbfa38822d62d2175a191c0ed360
Luis Osio Chico <72286405+LOsioChico@users.noreply.github.com>
2023-07-02 23:40:06 -0500

    style: Improve NavbarSearch layout and styling

--- src/presentation/shared/Layout/Navbar/NavbarSearch.tsx
+++ src/presentation/shared/Layout/Navbar/NavbarSearch.tsx
@@ -2,13 +2,15 @@ import { AiOutlineSearch } from 'react-icons/ai'
 
 export const NavbarSearch: React.FC = () => {
   return (
-    <div className='relative'>
+    <div className='flex items-center justify-center'>
       <input
         type='text'
-        className='h-11 select-none rounded-md border-2 border-neutral-200 border-opacity-10 bg-darkLight pl-3 pr-48 text-sm text-white outline-none duration-300 focus:border-white focus:border-opacity-50'
+        className='h-11 select-none rounded-l-lg border-2 border-neutral-200 border-opacity-10 bg-darkLight pl-3 pr-48 text-sm text-white outline-none ring-white duration-300 focus:border-white focus:border-opacity-50'
         placeholder='Search'
       />
-      <AiOutlineSearch className='text-neutro-50 absolute right-2 top-1/2 h-6 w-6 -translate-y-1/2 cursor-pointer' />
+      <div className='flex h-11 cursor-pointer items-center justify-center rounded-r-lg border-2 border-black border-opacity-10 bg-neutral-900 text-sm text-white duration-300 hover:border-white hover:border-opacity-50'>
+        <AiOutlineSearch className='text-neutro-50 h-8 w-8 cursor-pointer px-1' />
+      </div>
     </div>
   )
 }
