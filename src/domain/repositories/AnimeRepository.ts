bd2d869ff4b3a2337700913e0ecd8f615692d0c7
Luis Osio Chico <72286405+LOsioChico@users.noreply.github.com>
2023-07-02 23:43:20 -0500

    feat(repo): add getRecentEpisodes to AnimeRepository

--- src/domain/repositories/AnimeRepository.ts
+++ src/domain/repositories/AnimeRepository.ts
@@ -3,4 +3,5 @@ import { AnimeAPI } from '@/infrastructure/api/AnimeAPI'
 export const AnimeRepository = {
   getTrending: AnimeAPI.getTrending,
   getPopular: AnimeAPI.getPopular,
+  getRecentEpisodes: AnimeAPI.getRecentEpisodes,
 }
