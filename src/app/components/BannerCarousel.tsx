d28855ee45e92938d15a1e346837a76d1900bd65
Luis Osio Chico <72286405+LOsioChico@users.noreply.github.com>
2023-07-02 23:48:52 -0500

    feat: anime info added to banner

--- src/app/components/BannerCarousel.tsx
+++ src/app/components/BannerCarousel.tsx
@@ -1,6 +1,8 @@
 import { type Anime } from '@/domain/models'
+import { TbBookmark } from 'react-icons/tb'
 import { Autoplay } from 'swiper'
 import { Swiper, SwiperSlide } from 'swiper/react'
+import { MainButton } from '.'
 
 import 'swiper/css/bundle'
 
@@ -25,6 +27,22 @@ export const BannerCarousel: React.FC<BannerCarouselProps> = ({ anime }) => {
           {anime?.map((anime) => (
             <SwiperSlide key={anime.id}>
               <div className='flex h-full items-center justify-center'>
+                <div className='absolute left-44 top-24 z-10 flex h-1/2 flex-col gap-4 font-bold'>
+                  <h3 className='text-5xl font-bold text-white'>
+                    {anime.title.userPreferred}
+                  </h3>
+                  <p
+                    className='line-clamp-6 w-80 overflow-hidden overflow-ellipsis text-sm font-normal text-white'
+                    dangerouslySetInnerHTML={{ __html: anime.description }}
+                  />
+                  <div className='flex gap-4 text-sm'>
+                    <MainButton variant='white'>Learn More</MainButton>
+                    <MainButton variant='black'>
+                      <TbBookmark className='text-xl' />
+                      To Watch
+                    </MainButton>
+                  </div>
+                </div>
                 <img
                   src={anime.cover}
                   alt={anime.title.userPreferred}
@@ -32,6 +50,7 @@ export const BannerCarousel: React.FC<BannerCarouselProps> = ({ anime }) => {
                   loading='lazy'
                 />
                 <div className='absolute left-0 top-0 h-full w-full bg-gradient-to-t from-dark to-transparent' />
+                <div className='absolute left-0 top-0 h-1/2 w-full bg-gradient-to-b from-dark to-transparent' />
               </div>
             </SwiperSlide>
           ))}
