cd2aad8efd3ff902a4bd2b586f94bb2431d5185f
Luis Osio Chico <72286405+LOsioChico@users.noreply.github.com>
2023-07-02 23:49:46 -0500

    style: improve AnimeCardCarousel design

--- src/app/components/AnimeCardCarousel.tsx
+++ src/app/components/AnimeCardCarousel.tsx
@@ -1,8 +1,8 @@
 import { type Anime } from '@/domain/models/Anime'
 import { Swiper, SwiperSlide } from 'swiper/react'
-import { AnimeCard } from './common'
 
 import 'swiper/css/bundle'
+import { AnimeCard } from './common'
 
 interface CarouselProps {
   animes: Anime[]
@@ -16,15 +16,22 @@ export const AnimeCardCarousel: React.FC<CarouselProps> = ({
   return (
     <div className='w-5/6'>
       <h1 className='mb-2 ml-2 select-none text-xl'>{title}</h1>
-      <Swiper slidesPerView={6} data-testid='carousel'>
-        {animes?.map((anime) => (
-          <SwiperSlide key={anime.id}>
-            <div className='flex items-center justify-center'>
+      <div className='relative'>
+        <Swiper
+          slidesPerView={6}
+          centeredSlides={true}
+          centeredSlidesBounds={true}
+          data-testid='carousel'
+        >
+          {animes?.map((anime) => (
+            <SwiperSlide key={anime.id}>
               <AnimeCard anime={anime} />
-            </div>
-          </SwiperSlide>
-        ))}
-      </Swiper>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+        <div className='absolute left-0 top-0 z-10 h-full w-2 bg-gradient-to-r from-dark to-transparent' />
+        <div className='absolute right-0 top-0 z-10 h-full w-2 bg-gradient-to-l from-dark to-transparent' />
+      </div>
     </div>
   )
 }
