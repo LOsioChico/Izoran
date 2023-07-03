8888d8bc4d9d3e76abb00448797872449a5b5451
Luis Osio Chico <72286405+LOsioChico@users.noreply.github.com>
2023-07-02 23:40:49 -0500

    feat: Add getRecentEpisodes method to AnimeAPI

--- src/infrastructure/api/AnimeAPI.ts
+++ src/infrastructure/api/AnimeAPI.ts
@@ -1,8 +1,9 @@
+import { type AnimeRecentResponse, type AnimeResponse } from '@/domain/models'
 import { axiosInstance } from './axios'
 export const AnimeAPI = {
   getTrending: async (page = 1, perPage = 15) => {
     try {
-      const { data } = await axiosInstance.get('/trending', {
+      const { data } = await axiosInstance.get<AnimeResponse>('/trending', {
         params: {
           page,
           perPage,
@@ -16,12 +17,29 @@ export const AnimeAPI = {
 
   getPopular: async (page = 1, perPage = 15) => {
     try {
-      const { data } = await axiosInstance.get('/popular', {
+      const { data } = await axiosInstance.get<AnimeResponse>('/popular', {
         params: {
           page,
           perPage,
         },
       })
+      return data
+    } catch (error) {
+      if (error instanceof Error) throw new Error(error.message)
+    }
+  },
+
+  getRecentEpisodes: async (page = 1, perPage = 18) => {
+    try {
+      const { data } = await axiosInstance.get<AnimeRecentResponse>(
+        '/recent-episodes',
+        {
+          params: {
+            page,
+            perPage,
+          },
+        }
+      )
       return data
     } catch (error) {
       if (error instanceof Error) throw new Error(error.message)
