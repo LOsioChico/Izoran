896711a577e35fd7274b801fbd6b46f807d2d876
Luis Osio Chico <72286405+LOsioChico@users.noreply.github.com>
2023-07-04 15:35:14 -0500

    fix(AppRoutes): Fix conditional rendering of Auth component

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -3,9 +3,16 @@ import { useAuthStore } from '@/infrastructure/state'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { HomePage } from '../presentation/pages'
 import { Layout } from '../presentation/shared/Layout'
+import { useAuth } from '@/auth'
 
 export const AppRoutes: React.FC = () => {
+  useAuth()
   const { user } = useAuthStore()
+
+  if (user === null) {
+    return <div>Loading...</div>
+  }
+
   return (
     <BrowserRouter>
       {user === undefined && <Auth />}
