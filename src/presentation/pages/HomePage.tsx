685ed1322a592f656886c0a0933f04a6e09ebbd4
Luis Osio Chico <72286405+LOsioChico@users.noreply.github.com>
2023-07-05 23:16:00 -0500

    style: Remove unnecessary empty div

--- src/presentation/pages/HomePage.tsx
+++ src/presentation/pages/HomePage.tsx
@@ -4,7 +4,6 @@ import { PopularBanner, TrendingNow } from '@/app/containers/Carousels'
 export const HomePage: React.FC = () => {
   return (
     <>
-      <div className='pt-20' />
       <div className='relative'>
         <PopularBanner />
         <TrendingNow />
