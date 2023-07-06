de54eec788204e0bd99c5b5f210ffa33c45d6768
Luis Osio Chico <72286405+LOsioChico@users.noreply.github.com>
2023-07-05 23:12:04 -0500

    style: Update BannerCarousel component styles

--- src/app/components/BannerCarousel.tsx
+++ src/app/components/BannerCarousel.tsx
@@ -12,7 +12,7 @@ interface BannerCarouselProps {
 
 export const BannerCarousel: React.FC<BannerCarouselProps> = ({ anime }) => {
   return (
-    <div className='relative h-[60vh]'>
+    <div className='relative h-[60vh] min-h-[500px]'>
       <div className='h-full bg-red-50'>
         <Swiper
           modules={[Autoplay]}
@@ -22,20 +22,20 @@ export const BannerCarousel: React.FC<BannerCarouselProps> = ({ anime }) => {
             disableOnInteraction: false,
             pauseOnMouseEnter: true,
           }}
-          className='h-full'
+          className='h-full w-full'
         >
           {anime?.map((anime) => (
             <SwiperSlide key={anime.id}>
               <div className='flex h-full items-center justify-center'>
-                <div className='absolute left-44 top-24 z-10 flex h-1/2 flex-col gap-4 font-bold'>
-                  <h3 className='text-5xl font-bold text-white'>
+                <div className='absolute left-44 top-24 z-10 flex h-1/2 flex-col gap-4 font-bold 5xl:left-36 xl:left-28 xs:left-16'>
+                  <h3 className='text-5xl font-bold text-white 5xl:text-5xl 4xl:text-4xl xl:text-3xl xs:text-2xl'>
                     {anime.title.userPreferred}
                   </h3>
                   <p
-                    className='line-clamp-6 w-80 overflow-hidden overflow-ellipsis text-sm font-normal text-white'
+                    className='line-clamp-6 w-96 max-w-[75ch] overflow-hidden overflow-ellipsis text-sm font-normal text-white xs:w-72'
                     dangerouslySetInnerHTML={{ __html: anime.description }}
                   />
-                  <div className='flex gap-4 text-sm'>
+                  <div className='flex gap-4 text-sm xs:ml-6'>
                     <MainButton variant='white'>Learn More</MainButton>
                     <MainButton variant='black'>
                       <TbBookmark className='text-xl' />
