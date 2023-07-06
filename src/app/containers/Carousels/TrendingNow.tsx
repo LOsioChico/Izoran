cea0e3b092e5f42c07da5dd39e2e58e801089795
Luis Osio Chico <72286405+LOsioChico@users.noreply.github.com>
2023-07-05 23:12:39 -0500

    style: update carousel styling for mobile view

--- src/app/containers/Carousels/TrendingNow.tsx
+++ src/app/containers/Carousels/TrendingNow.tsx
@@ -6,7 +6,7 @@ export const TrendingNow: React.FC = () => {
   if (trending === undefined) return null
 
   return (
-    <div className='relative bottom-20 z-10 flex w-full items-center justify-center px-8 pb-6'>
+    <div className='relative bottom-20 z-10 flex w-full items-center justify-center px-8 pb-6 xs:px-0'>
       <AnimeCardCarousel animes={trending} title='Trending Now' />
     </div>
   )
