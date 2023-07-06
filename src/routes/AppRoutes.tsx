0f93747ff870300da42bc6619a7f4b8c19c10b92
Luis Osio Chico <72286405+LOsioChico@users.noreply.github.com>
2023-07-05 23:15:48 -0500

    feat: Add loading spinner and not found page

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,16 +1,21 @@
 import { Auth } from '@/app/auth'
 import { useAuthStore } from '@/infrastructure/state'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import { HomePage } from '../presentation/pages'
+import { HomePage, NotFound, SearchPage } from '../presentation/pages'
 import { Layout } from '../presentation/shared/Layout'
 import { useAuth } from '@/auth'
+import { ImSpinner9 } from 'react-icons/im'
 
 export const AppRoutes: React.FC = () => {
   useAuth()
   const { user } = useAuthStore()
 
   if (user === null) {
-    return <div>Loading...</div>
+    return (
+      <div className='flex h-screen items-center justify-center'>
+        <ImSpinner9 className='h-12 w-12 animate-spin' />
+      </div>
+    )
   }
 
   return (
@@ -19,7 +24,9 @@ export const AppRoutes: React.FC = () => {
       <Routes>
         <Route element={<Layout />}>
           <Route path='/' element={<HomePage />} />
+          <Route path='/search/:query' element={<SearchPage />} />
         </Route>
+        <Route path='*' element={<NotFound />} />
       </Routes>
     </BrowserRouter>
   )
